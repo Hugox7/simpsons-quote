df77c6259142975c7bde1a8c1b2382c1c1c2045d
Hugo Trichard <hugo.trichard@gmail.com>
2018-10-04 12:03:04 +0200

    ajout pour quete react state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Lamp from "./Lamp";
+import Quote from "./Quote";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      on: false
+    };
+  }
+  handleClick = () => {
+    this.setState({ on: !this.state.on });
+  };
+
   render() {
+    const worklogo = this.state.on ? "work" : "break";
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className={worklogo} alt="logo" />
+          <h1 className="App-title">Simpson Quotes</h1>
         </header>
+        <Lamp on />
+        <Lamp />
+        <Quote
+          quote="I believe the children are the future... Unless we stop them now!"
+          character="Homer Simpson"
+          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+        />
+
+        <button onClick={this.handleClick}>Working</button>
       </div>
     );
   }
